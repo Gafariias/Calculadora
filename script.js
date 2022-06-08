f5e97a345148c6c3abd408a6d0a4965171ae92cf
Gabriel Farias <gabriel_ff@outlook.com>
2022-06-08 19:07:32 -0300

    Interação com as teclas e bugs visuais

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
-const tela = document.querySelector('#tela');
-const numeros = document.querySelectorAll('.num');
-const operadores = document.querySelectorAll('.operacao');
-const igualBtn = document.querySelector('#igual');
-const deletaBtn = document.querySelector('#CE'); 
-const limpaBtn = document.querySelector('#C'); 
-
-let novoNum = true;
-let operador;
-let numAnterior
+//Obter as tags HTML
+var tela = document.querySelector('#tela');
+var botoes = document.querySelectorAll('.tecla');
+var novoNum = true;
 
 const attTela = (texto) => {
-    tela.textContent += texto
-}
-const inserirNum = (evento) => attTela(evento.target.textContent);
-numeros.forEach (num => num.addEventListener('click', inserirNum));
+        if (novoNum) {
+            tela.innerHTML = "";
+            novoNum = false
+        }
 
-const inserirOp = (evento) =>attTela(evento.target.textContent);
-operadores.forEach (op => op.addEventListener('click', inserirOp))
-
-const calcular = () => {
-    const expressao = tela.textContent;
-    expressao = Number(expressao);
+    tela.innerHTML += texto;
 }
 
-const resultado = (calcular) => calcular(calcular.target.textContent);
-igualBtn.forEach (igual => igual.addEventListener('click', calcular));
+const resultado = () => {
+    novoNum = true;
+    let valor = tela.innerHTML;
+    let equacao = valor
 
-console.log(resultado)
-
-/* const attTela = (texto) => {
-    if (novoNum) {
-        tela.textContent = texto;
-        novoNum = false
-    } else {
-        tela.textContent += texto;
+    if (equacao) {
+        try {
+            tela.innerHTML = eval(equacao);
+            
+        } catch (error) {
+            tela.innerHTML = "ERRO"
+        }
     }
+    
+    
 }
-const inserirNum = (evento) => attTela(evento.target.textContent);
-numeros.forEach (num => num.addEventListener('click', inserirNum));
 
-const selecionarOperadores = (evento) => {
-    if (!novoNum) {
-        calcular();
-        novoNum = true;
-        operador = evento.target.textContent;
-        numAnterior = tela.textContent;
-
-        console.log(operador)
-        console.log(numAnterior)
-    }
+function clique() { //Aqui eu optei por função ao invés de arrow apenas para praticar com os dois e deixei as arrow acima por organização
+    const inserir = (evento) => attTela(evento.target.textContent);
+    botoes.forEach (btn => btn.addEventListener('click', inserir));
+    
+    
 }
-operadores.forEach (num => num.addEventListener('click', selecionarOperadores));
-*/
-console.log(tela)
-console.log(numeros)
-console.log(operadores)
-console.log(igualBtn)
-console.log(deletaBtn)
-console.log(limpaBtn)
-console.log(novoNum)
+
+function limpar() {
+    tela.textContent = "";
+}
+
+function corrige() {
+    let conteudo = tela.innerHTML
+    conteudo = conteudo.slice(0, -1)
+    tela.innerHTML = conteudo
+}
+
+
+
+clique();
